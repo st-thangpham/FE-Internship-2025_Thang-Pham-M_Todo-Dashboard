f6f327c7f0d1a103e523ec3e860c10e94f4a6fdc
st-thangpham <thang.pham@supremetech.vn>
2025-04-26 13:00:26 +0700

    Create header and sidebar

--- dashboard-todo-app/src/shared/components/layout/Sidebar.tsx
+++ dashboard-todo-app/src/shared/components/layout/Sidebar.tsx
@@ -1,13 +1,21 @@
-// src/app/shared/components/Sidebar.tsx
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
+import { RootState } from '@/shared/redux/store';
 import { logout } from '@/shared/redux/auth/authActions';
 
+import DashboardIcon from '@/assets/icons/icon-dashboard.svg';
+import DashboardActive from '@/assets/icons/icon-dashboard-active.svg';
+import MyTaskIcon from '@/assets/icons/icon-mytask.svg';
+import MyTaskActive from '@/assets/icons/icon-mytask-active.svg';
+import LogoutIcon from '@/assets/icons/icon-logout.svg';
+
 const Sidebar: React.FC = () => {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const user = useSelector((state: RootState) => state.auth.loginAccount);
 
   const handleLogout = () => {
     dispatch(logout());
@@ -16,8 +24,44 @@ const Sidebar: React.FC = () => {
 
   return (
     <aside className="sidebar">
-      <button className="btn btn-logout" onClick={handleLogout}>
-        Logout
+      <div className="sidebar-user">
+        <p className="sidebar-name">{user.fullname}</p>
+        <p className="sidebar-email">{user.email}</p>
+      </div>
+      <nav className="sidebar-nav">
+        <NavLink
+          to="/dashboard"
+          className={({ isActive }) =>
+            `sidebar-link ${isActive ? 'sidebar-link-active' : ''}`
+          }
+        >
+          {({ isActive }) => (
+            <>
+              <img
+                src={isActive ? DashboardActive : DashboardIcon}
+                alt="Dashboard"
+              />
+              <span>Dashboard</span>
+            </>
+          )}
+        </NavLink>
+        <NavLink
+          to="/mytask"
+          className={({ isActive }) =>
+            `sidebar-link ${isActive ? 'sidebar-link-active' : ''}`
+          }
+        >
+          {({ isActive }) => (
+            <>
+              <img src={isActive ? MyTaskActive : MyTaskIcon} alt="My Task" />
+              <span>My Task</span>
+            </>
+          )}
+        </NavLink>
+      </nav>
+      <button className="sidebar-logout" onClick={handleLogout}>
+        <img src={LogoutIcon} alt="Icon Logout" />
+        <span>Logout</span>
       </button>
     </aside>
   );
